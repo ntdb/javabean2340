ee5431a026f05e42bf01e375a987e8b95cd0f689
nathanaelbailey <nathanaelbailey@gmail.com>
2011-03-04 04:49:43 +0000

    Basic persistence working

--- JavaBean/src/IOController.java
+++ JavaBean/src/IOController.java
@@ -6,17 +6,35 @@ public class IOController {
 	{
 		XStream xstream = new XStream();
 		String XMLOut = xstream.toXML(obj);
-		
-		File file = new File("write.txt"); //obj + ".XML");
-		Writer writer = new BufferedWriter(new FileWriter(file));
+		Writer writer = null;
+		File file = new File(obj + ".XML");
+		writer = new BufferedWriter(new FileWriter(file));
+//		FileWriter writer = new FileWriter(file);
 		writer.write(XMLOut);
+		writer.close();
+		System.out.println(XMLOut);
 	}
 	
-	public static Object getFromXML(Object obj)
+	public static Object getFromXML(Object obj) throws IOException
 	{
 		XStream xstream = new XStream();
-		
-		Object fromXML = xstream.fromXML("write.txt"); //obj + ".XML");
-		return fromXML;
+
+		if(new File(obj + ".XML").exists())			//Checks if XML already exists
+		{
+			byte[] buffer = new byte[(int) new File(obj + ".XML").length()];
+			FileInputStream f = new FileInputStream(obj + ".XML");
+			f.read(buffer);
+			if(obj instanceof UserController)
+			{
+				UserController fromXML = (UserController)xstream.fromXML(new String(buffer));
+				return fromXML;
+			}
+		}
+		else							//Returns new object if necessary
+		{
+			if(obj instanceof UserController)
+				return new UserController();
+		}
+		return null;
 	}
 }
