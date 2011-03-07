98ec2aff7e32ff98c7189eadfc5a216969e53c62
halflifedave@gmail.com <halflifedave@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-07 16:27:06 +0000

    LoginClass added

---
+++ trunk/JavaBean/src/LoginClass.java
@@ -0,0 +1,27 @@
+package JavaBean.src;
+
+public class LoginClass 
+{
+	private int maxattempts;
+	private int usercount;
+	private int[] userIds;
+	public User userlogin;
+	
+	/**
+	 * LoginClass constructor. 
+	 * Initialize counting values to 0.
+	 */
+	public LoginClass()
+	{
+		maxattempts = 0;
+		usercount = 0;
+	}
+	
+	public int getUsercount()
+	{
+		return usercount;
+	}
+	
+	
+	
+}
