6dd36f1fe335b5df35a75e33a7b0ee2ec6b0a7d6
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-10 22:02:30 +0000

    CRUDed and Javadoc'd Doctor

--- trunk/JavaBean/src/Doctor.java
+++ trunk/JavaBean/src/Doctor.java
@@ -1,25 +1,53 @@
 
+/**
+ * This class represents a Doctor in the Hospital System that extends from
+ * Nurse with added doctor responsibilities and abilities.
+ *
+ */
 public class Doctor extends Nurse{
 
 	private int[] appointmentIDs;
 	
+	/**
+	 * Default constructor sets all attributes to null
+	 * 
+	 */
 	public Doctor(){
 		super();
 		appointmentIDs = null;
 	}//end default Doctor constructor
 	
+	/**
+	 * Overloaded constructor sets all Doctor attributes
+	 * 
+	 * @param name The name of the Doctor
+	 * @param password The Doctor's password
+	 * @param permissions The Doctor's permissions
+	 * @param userID The Doctor's user ID
+	 * @param appointmentIDs The Doctor's Appointment IDs
+	 */
 	public Doctor(String name, String password, int permissions, int userID,
 			int[] appointmentIDs){
 		super(name, password, permissions, userID);
 		this.appointmentIDs = appointmentIDs;
 	}//end Doctor constructor
 	
+	/**
+	 * Adds the Doctor's orders to the given Patient's file
+	 * 
+	 * @param patient The patient receiving orders
+	 */
 	public void addDoctorsOrders(Patient patient){
-		
+		//patient.getMedicalHistory().setDoctorsOrders(); //bad OO?? 
 	}//end addDoctorsOrders
 	
+	/**
+	 * Verifies that the patient-requested appointment is available
+	 * 
+	 * @return The true or false value whether the Appointment is available
+	 */
 	public boolean verifyAppointment(){
-		
+		//verifies Appointment
 		return false;
 	}//end verifyAppointment
 	
