06eb2ab76e312fd05644dbe3a36de884e6f79972
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-14 17:54:48 +0000

    comment for Treatment finished

--- trunk/JavaBean/src/Treatment.java
+++ trunk/JavaBean/src/Treatment.java
@@ -1,3 +1,8 @@
+/**
+ * This class represents a treatment record of patients, on a per appointment basis.
+ * @author William
+ */
+
 public class Treatment 
 {
 	private int patientID;
@@ -5,6 +10,13 @@ public class Treatment
 	private int appointmentID;
 	private String details;
 	
+	/**
+	 * The standard constructor of the treatment class, parameters can be set to null.
+	 * @param patientID The user ID of the patient
+	 * @param doctorID The user ID of the doctor who conduct the service
+	 * @param appointmentID The ID number of the related appointment. Best way to trace the treatment.
+	 * @param details The detailed instruction of the treatment.
+	 */
 	private Treatment(int patientID, int doctorID, int  appointmentID, String details)
 	{
 		this.patientID = patientID;
@@ -13,6 +25,11 @@ public class Treatment
 		this.details = details;
 	}
 	
+	/**
+	 * This method is used to fill in the details. 
+	 * If the detail is not empty, it will strat a new line and attach the new details.
+	 * @param newDetails The detailed instructions.
+	 */
 	public void createDetails(String newDetails)
 	{
 		if(details==null)
@@ -25,6 +42,11 @@ public class Treatment
 		}
 	}
 	
+	/**
+	 * This method will export the treatment as a string.
+	 * It will start a new line and end with a new line.
+	 * @return A string contains all the infomation of the treatment.
+	 */
 	public String toString()
 	{
 		String fullInfo = "\nPatient ID: " + patientID +
