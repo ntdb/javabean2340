77147ace7d3ddf640ae604da7be49f1edbe6fc67
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-14 18:07:51 +0000

    MedicalHistory comments finished

--- trunk/JavaBean/src/MedicalHistory.java
+++ trunk/JavaBean/src/MedicalHistory.java
@@ -1,31 +1,64 @@
 import java.util.ArrayList;
 
+/**
+ * This class represents the medical history of the patient
+ * @author William
+ *
+ */
 public class MedicalHistory 
 {
 	private ArrayList<Integer> appointmentIDs;
 	private ArrayList<Treatment> treatments;
 	private ArrayList<DoctorsOrders> doctorsOrders;
 	
+	/**
+	 * This is the default constructor of the MedicalHistory class
+	 */
+	public MedicalHistory()
+	{
+		appointmentIDs = new ArrayList<Integer>();
+		treatments = new ArrayList<Treatment>();
+		doctorsOrders = new ArrayList<DoctorsOrders>();
+	}
+	
 	public void getWeightChart()
 	{
 		//return null;
 	}// need implementation
 	
+	/**
+	 * This method will get the appointmentID of the appointment and add the ID into the records
+	 * @param newAppointment The appointment needed to be added into the Medical History
+	 */
 	public void addAppointment(Appointment newAppointment)
 	{
 		appointmentIDs.add( newAppointment.getAppointmentID() );
 	}
 	
+	/**
+	 * This method will add the new treatment into the medical history
+	 * @param newTreatment The treatment needed to be added into the medical history
+	 */
 	public void addTreatment( Treatment newTreatment )
 	{
 		treatments.add(newTreatment);
 	}
 	
+	/**
+	 * This method will add the new Doctors' Orders into the medical history of the patient
+	 * @param newDoctorsOrders The Doctors' Orders needed to be added into the medical history
+	 */
 	public void addDoctorsOrders( DoctorsOrders newDoctorsOrders)
 	{
 		doctorsOrders.add(newDoctorsOrders);
 	}
 	
+	/**
+	 * This method will export the medical history as a string.
+	 * It will start a new line and end with a new line.
+	 * @param appSchedule The schedule is required to get the information of appointments
+	 * @return A string contains all the information of the medical history
+	 */
 	public String toString(Schedule appSchedule)
 	{
 		String appointmentString = new String();
@@ -47,7 +80,7 @@ public class MedicalHistory
 			doctorsOrdersString = doctorsOrdersString + doctorsOrders.get(i).toString();
 		}
 		
-		String fullInfo = "Medical History\n\n\nAppointments:\n" + appointmentString +
+		String fullInfo = "\nMedical History\n\n\nAppointments:\n" + appointmentString +
 						  "\nTreatments:\n" + treatmentString +
 						  "\nDoctor's Orders:\n" + doctorsOrdersString;
 		return fullInfo;
