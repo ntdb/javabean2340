92490e4682d7e4590ec6fb0ee7acd6528e8e62e7
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-14 17:55:11 +0000

    comment for DoctorsOrders finished

--- trunk/JavaBean/src/DoctorsOrders.java
+++ trunk/JavaBean/src/DoctorsOrders.java
@@ -1,3 +1,8 @@
+/**
+ * This class represents doctors' orders received by the patient
+ * @author William
+ *
+ */
 public class DoctorsOrders 
 {
 	private String prescription;
@@ -5,6 +10,13 @@ public class DoctorsOrders
 	private String followUp;
 	private String other;
 	
+	/**
+	 * The standard constructor of the class, parameters can be set to null.
+	 * @param prescription The prescription of the patient
+	 * @param labWork The lab work involved in the service
+	 * @param followUp The things need to be done after the service
+	 * @param other Additional infomation
+	 */
 	public DoctorsOrders(String prescription, String labWork, String followUp, String other)
 	{
 		this.prescription = prescription;
@@ -13,6 +25,11 @@ public class DoctorsOrders
 		this.other = other;
 	}
 	
+	/**
+	 * This method will export the doctors' orders as a string.
+	 * It will start a new line and end with a new line.
+	 * @return A string contains all the infomation of the doctors' order.
+	 */
 	public String toString()
 	{
 		String fullInfo = "\nPrescription: " + prescription +
