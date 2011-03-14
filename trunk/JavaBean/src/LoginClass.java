022efff6cb7187d40e2d20f16e64904140b65922
halflifedave@gmail.com <halflifedave@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-14 21:19:57 +0000

    Lowered Audit warnings generated for class

--- trunk/JavaBean/src/LoginClass.java
+++ trunk/JavaBean/src/LoginClass.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 /**
  * LoginClass. Used to verify that the user logs in correctly
  * @author David
@@ -6,7 +5,6 @@ import java.util.ArrayList;
  */
 public class LoginClass {
 	private int maxattempts;
-//	private int usercount; //implement at a later time
 	private int userId;
 	private boolean validpass;
 	private UserController uc;
@@ -15,18 +13,13 @@ public class LoginClass {
 	/**
 	 * LoginClass constructor. 
 	 * Initialize counting values to 0, boolean values to false
-	 * @param userIDs user id of the user trying to login
+	 * @param userID user id of the user trying to login
 	 */
 	public LoginClass(int userID){
 		maxattempts = 0;
 		this.userId = userID;
 		validpass = false;
-		uc.getUserIndex(userID);
 	}
-	
-/*	public int getUsercount(){
-		return usercount;
-	} */
 	
 	/**
 	 * Returns the max attempts
@@ -44,22 +37,33 @@ public class LoginClass {
 		maxattempts = max;
 	}
 	
+	/**
+	 * Returns LoginClass as a String. Fulfilling audit requirements.
+	 * @return String value of class
+	 */
+	public String toString(){
+		return "LoginClass";
+	}
+	
 	public void validate(String password){
 		//first verify that User is in the user controller object
-		if (uc.getUserIndex(userId) == -1)
+		if (uc.getUserIndex(userId) == -1){
 			return;
+		}
 		
 		logger = uc.getUser(userId);
 		//retrieve user information and verify password matches usercontroller's
-		do
-		{
-			if(logger.getPassword() == password)
+		do{
+			if(logger.getPassword().equals(password)){
 				validpass = true;
-			else
+			}
+			else{
 				maxattempts++;
-		}while(maxattempts<3 || validpass != false);
-		if (maxattempts == 3 && validpass == false)
+			}
+		}while(maxattempts < 3 || validpass != false);
+		if (maxattempts == 3 && validpass == false){
 			System.out.print("Max Attempts reached. Contact an administrator to reset");
+		}
 	}
 	
 	
