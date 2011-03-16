9ce68dbb038b9b09c76d53557e88124c85915b06
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-16 06:45:24 +0000

    Code Audit on Doctor and Patient

--- trunk/JavaBean/src/Doctor.java
+++ trunk/JavaBean/src/Doctor.java
@@ -1,11 +1,24 @@
+/* Doctor class for Hospital System
+ * Team Java Bean
+ */
 
 /**
  * This class represents a Doctor in the Hospital System that extends from
  * Nurse with added doctor responsibilities and abilities.
  *
+ * @author Team Java Bean
+ * @version 1.1 3/14/11
  */
 public class Doctor extends Nurse{
+	
+	/**
+	 * The permission level of all Nurses, used to generate user ID
+	 */
+	private static final int PERMISSIONS = 2;
 
+	/**
+	 * An array of IDs for appointments that the doctor has
+	 */
 	private int[] appointmentIDs;
 	
 	/**
@@ -13,7 +26,6 @@ public class Doctor extends Nurse{
 	 * 
 	 */
 	public Doctor(){
-		super();
 		appointmentIDs = null;
 	}//end default Doctor constructor
 	
@@ -27,15 +39,24 @@ public class Doctor extends Nurse{
 	public Doctor(String name, String password, int[] appointmentIDs){
 		super(name, password);
 		this.appointmentIDs = appointmentIDs;
+		//deep copy apointmentIDs
+		userID = generateUserID(PERMISSIONS);
 	}//end Doctor constructor
 	
 	/**
 	 * Adds the Doctor's orders to the given Patient's file
 	 * 
 	 * @param patient The patient receiving orders
+	 * @param prescription The prescription of the doctors orders to be added
+	 * @param labWork The lab work involved in the service
+	 * @param followUp The procedures needed to be done after the service
+	 * @param other Additional information
 	 */
-	public void addDoctorsOrders(Patient patient){
-		//patient.getMedicalHistory().setDoctorsOrders(); //bad OO?? 
+	public void addDoctorsOrders(Patient patient, String prescription, String labWork,
+			String followUp, String other){
+		final DoctorsOrders newDoctorsOrders = new DoctorsOrders(prescription, labWork,
+				followUp, other);
+		patient.getMedicalHistory().addDoctorsOrders(newDoctorsOrders); //bad OO?? 
 	}//end addDoctorsOrders
 	
 	/**
@@ -43,7 +64,7 @@ public class Doctor extends Nurse{
 	 * 
 	 * @return The true or false value whether the Appointment is available
 	 */
-	public boolean verifyAppointment(){
+	public boolean canMakeAppointment(){
 		//verifies Appointment
 		return false;
 	}//end verifyAppointment
