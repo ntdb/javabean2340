8521416a7c369915d72c826ffb7d962bb253d9a2
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-16 05:45:14 +0000

    Code Audit on Nurse class. Changed a method call in HospitalTest whose signature was changed due to audit.

--- trunk/JavaBean/src/Nurse.java
+++ trunk/JavaBean/src/Nurse.java
@@ -1,18 +1,28 @@
+/* Nurse Class for Hospital System
+ * Team Java Bean
+ */
+
 import java.util.Date;
 
 /**
  * This class represents a Nurse in the Hospital system and is a subclass 
  * of User that provides the basic functions necessary in a Hospital setting
  *
+ * @author Team Java Bean
+ * @version 1.1 3/14/11
  */
 public class Nurse extends User {
 
+	/**
+	 * The permission level of all Nurses, used to generate user ID
+	 */
+	private static final int PERMISSIONS = 3;
+	
 	/**
 	 * Default constructor sets all attributes to null
 	 * 
 	 */
 	public Nurse(){
-		super();
 	}//end default Nurse constructor
 	
 	/**
@@ -23,7 +33,7 @@ public class Nurse extends User {
 	 */
 	public Nurse(String name, String password){
 		super(name, password);
-		userID = generateUserID(3);
+		userID = generateUserID(PERMISSIONS);
 	}//end Nurse constructor
 	
 	/**
@@ -32,13 +42,18 @@ public class Nurse extends User {
 	 * @return The new patient's userID
 	 */
 	public int createPatient(){
-		Patient newPatient = new Patient();
+		final Patient newPatient = new Patient();
 		UserController.addUser(newPatient);
 		return newPatient.getUserID();
 	}//end createPatient
 	
-	public void deletePatient(int UserID){
-		UserController.deleteUser(UserID);		
+	/**
+	 * Deletes a Patient from the Hospital system
+	 * 
+	 * @param userID The user ID of the Patient to be deleted
+	 */
+	public void deletePatient(int userID){
+		UserController.deleteUser(userID);
 	}//end deletePatient
 	
 	/**
@@ -64,7 +79,7 @@ public class Nurse extends User {
 	 * @param name The name of the Patient to lookup
 	 * @return The User looked-up
 	 */
-	public User PatientLookupByName(String name){
+	public User patientLookupByName(String name){
 		return UserController.getUser(name);
 	}//end PatientLookupByName
 	
@@ -74,7 +89,8 @@ public class Nurse extends User {
 	 * @param patient The Patient to look at
 	 */
 	public void viewPatient(Patient patient){
-		//Views Patient information
+		//Views Patient information, implement later
+		int id = patient.getUserID();
 	}//end viewPatient
 	
 	/**
@@ -83,17 +99,36 @@ public class Nurse extends User {
 	 * @param schedule The schedule of Appointments
 	 * @param appTime The Date of the Appointment
 	 */
-	public void viewAppointments(Schedule schedule, Date appTime)
-	{
+	public void viewAppointments(Schedule schedule, Date appTime){
 		schedule.displayOneDaySchedule(appTime);
 	}//end viewAppointments
 
+	/**
+	 * Returns a count of the Users in the Hospital system
+	 * 
+	 * @return The number of Users in the system
+	 */
 	public int getUserCount() {
 		return UserController.getUserCount();
 	}
 
+	/**
+	 * Gets a Patient using the Patient's user ID
+	 * 
+	 * @param userID The user ID of the patient to get
+	 * @return The Patient
+	 */
 	public Patient getPatient(int userID) {
-		return (Patient)UserController.getUser(userID);
-	}
+		return (Patient) UserController.getUser(userID);
+	}//end getPatient
+	
+	/**
+	 * Returns a String representation of Nurse
+	 *  
+	 * @return A String representation of Nurse
+	 */
+	public String toString(){
+		return new String("Nurse: " + name);
+	}//end toString
 	
 }//end class Nurse
