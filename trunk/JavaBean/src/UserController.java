55c615d1e683c567065fcd1235b1710d320392d0
pyroalienmaniac@gmail.com <pyroalienmaniac@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-03-18 18:38:24 +0000

    Code Audit on Medical History and UserController

--- trunk/JavaBean/src/UserController.java
+++ trunk/JavaBean/src/UserController.java
@@ -1,38 +1,71 @@
+/* UserController class for Hospital System
+ * Team Java Bean
+ */
+
 import java.util.ArrayList;
 import java.io.IOException;
 
+/**
+ * 
+ * 
+ * @author Team Java Bean
+ * @version 1.1 3/14/11
+ */
 public class UserController {
 
+	/**
+	 * The List of Users currently using the system
+	 */
 	private static ArrayList<User> users = new ArrayList<User>();
 	
+	/**
+	 * Returns a User with the given user ID
+	 * 
+	 * @param userID The ID of the User to find
+	 * @return The found User
+	 */
 	public static User getUser(int userID) {
 		
-		for(int i=0; i<users.size(); i++) {
-			User user = (User) users.get(i);
-			if(user.getUserID() == userID)
+		for(int i=0; i < users.size(); i++) {
+			User user = users.get(i);
+			if(user.getUserID() == userID){
 				return user;
+			}
 		}
 		return null;
 	}
 	
+	/**
+	 * Returns a User by the given name
+	 * 
+	 * @param name The Name of the User to find
+	 * @return The found User
+	 */
 	public static User getUser(String name){
 		User user = null;
 		
 		for(int i = 0; i < users.size(); i++){
 			if(users.get(i).getName().equalsIgnoreCase(name)){
 				user = users.get(i);
-				i = users.size();
+				break;
 			}//end if statement
 		}//end for loop
 		
 		return user;
 	}//end overloaded getUser method (look-up by name)
 	
+	/**
+	 * Returns the index of a User by the given ID
+	 * 
+	 * @param userID The user ID of the User index to find
+	 * @return The index of the found User
+	 */
 	public static int getUserIndex(int userID) {
-		for(int i=0; i<users.size(); i++) {
-			User user = (User) users.get(i);
-			if(user.getUserID() == userID)
+		for(int i=0; i < users.size(); i++) {
+			User user = users.get(i);
+			if(user.getUserID() == userID){
 				return i;
+			}
 		}
 		return -1;
 	}
@@ -44,22 +77,39 @@ public class UserController {
 		return user.getPermissions();
 	}
 */	
+	/**
+	 * @param userIn
+	 */
 	public static void addUser(User userIn) {
 		users.add(userIn);
 	}
 	
+	/**
+	 * @param userID
+	 */
 	public static void deleteUser(int userID) {
 		users.remove(getUserIndex(userID));
 	}
 
+	/**
+	 * Returns the count of the Users in the system
+	 * 
+	 * @return The User count
+	 */
 	public static int getUserCount() {
 		return users.size();
 	}
 
+	/**
+	 * @throws IOException
+	 */
 	public static void save() throws IOException {
 		IOController.writeToXML(users);
 	}
 
+	/**
+	 * @throws IOException
+	 */
 	public static void load() throws IOException {
 		users = IOController.getFromXML(new Patient());
 	}
