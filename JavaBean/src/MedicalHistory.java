7b278efffabb27b62ffc6936d68014cf6a415780
pyroalienmaniac <pyroalienmaniac@gmail.com>
2011-03-18 18:38:24 +0000

    Code Audit on Medical History and UserController

--- JavaBean/src/MedicalHistory.java
+++ JavaBean/src/MedicalHistory.java
@@ -1,14 +1,31 @@
+/* MedicalHistory class for Hospital System
+ * Team Java Bean
+ */
+
 import java.util.ArrayList;
 
 /**
  * This class represents the medical history of the patient
  *
+ * @author Team Java Bean
+ * @version 1.1 3/14/11
  */
 public class MedicalHistory {
+	
+	
+	/**
+	 * A List of Appointment IDs
+	 */
 	private ArrayList<Integer> appointmentIDs;
 
+	/**
+	 * A List of Treatments
+	 */
 	private ArrayList<Treatment> treatments;
 	
+	/**
+	 * A list of DoctorOrders
+	 */
 	private ArrayList<DoctorsOrders> doctorsOrders;
 	
 	/**
@@ -20,12 +37,18 @@ public class MedicalHistory {
 		doctorsOrders = new ArrayList<DoctorsOrders>();
 	}
 	
+	/**
+	 * Returns weight information
+	 * 
+	 * @return Weight information
+	 */
 	public String getWeightChart() {
 		return null;
 	}// need implementation
 	
 	/**
-	 * This method will get the appointmentID of the appointment and add the ID into the records
+	 * This method will get the appointmentID of the appointment and add 
+	 * the ID into the records
 	 * @param newAppointment The appointment needed to be added into the Medical History
 	 */
 	public void addAppointment(Appointment newAppointment) {
@@ -46,10 +69,11 @@ public class MedicalHistory {
 	 * @return If found, return index, not found, return -1
 	 */
 	private int getTreatmentIndex(int appointmentID) {
-		for(int i = 0;i<treatments.size();i++) {
+		for(int i = 0;i < treatments.size();i++) {
 			Treatment theTreatment = treatments.get(i);
-			if(theTreatment.getAppointmentID() == appointmentID)
+			if(theTreatment.getAppointmentID() == appointmentID){
 				return i;
+			}
 		}
 		return -1;
 	}
@@ -60,7 +84,7 @@ public class MedicalHistory {
 	 * @return The String containing all the information of the treatment
 	 */
 	public String viewTreatment(int appointmentID) {
-		int index = getTreatmentIndex(appointmentID);
+		final int index = getTreatmentIndex(appointmentID);
 		return (treatments.get(index)).toString();
 	}
 	
@@ -70,7 +94,7 @@ public class MedicalHistory {
 	 * @param newDetails The details needed to add to the treatment
 	 */
 	public void updateTreatment(int appointmentID, String newDetails) {
-		int index = getTreatmentIndex(appointmentID);
+		final int index = getTreatmentIndex(appointmentID);
 		(treatments.get(index)).updateDetails(newDetails);
 	}
 	
@@ -79,37 +103,67 @@ public class MedicalHistory {
 	 * @param appointmentID The ID number of the appointment to find the treatment
 	 */
 	public void deleteTreatment(int appointmentID) {
-		int index = getTreatmentIndex(appointmentID);
+		final int index = getTreatmentIndex(appointmentID);
 		treatments.remove(index);
 	}
 
+	/**
+	 * Returns the count of the Treatments
+	 * 
+	 * @return The Treatment count
+	 */
 	public int getTreatmentCount() {
 		return treatments.size();
 	}
 	
 	/**
-	 * This method will add the new Doctors' Orders into the medical history of the patient
-	 * @param newDoctorsOrders The Doctors' Orders needed to be added into the medical history
+	 * This method will add the new Doctors' Orders into the medical 
+	 * history of the patient
+	 * @param newDoctorsOrders The Doctors' Orders needed to be added 
+	 *        into the medical history
 	 */
 	public void addDoctorsOrders( DoctorsOrders newDoctorsOrders) {
 		doctorsOrders.add(newDoctorsOrders);
 	}
 	
+	/**
+	 * Returns the doctor's orders
+	 * 
+	 * @return The doctor's orders
+	 */
 	public String viewDoctorsOrders() {
-		int latestOne = doctorsOrders.size()-1;
+		final int latestOne = doctorsOrders.size() - 1;
 		return (doctorsOrders.get(latestOne)).toString();
 	}
 	
-	public void updateDoctorsOrders(String prescription, String labWork, String followUp, String other) {
-		int latestOne = doctorsOrders.size()-1;
-		(doctorsOrders.get(latestOne)).updateContent(prescription, labWork, followUp, other);
+	/**
+	 * Updates the doctor's orders
+	 * 
+	 * @param prescription The prescription to be updated
+	 * @param labWork The lab work to be updated
+	 * @param followUp The follow up to be updated
+	 * @param other Additional information or orders to be updated
+	 */
+	public void updateDoctorsOrders(String prescription, String labWork,
+			String followUp, String other) {
+		final int latestOne = doctorsOrders.size() - 1;
+		(doctorsOrders.get(latestOne)).updateContent(prescription, labWork,
+				followUp, other);
 	}
 	
+	/**
+	 * Deletes the DoctorsOrders
+	 */
 	public void deleteDoctorsOrders() {
-		int latestOne = doctorsOrders.size()-1;
+		final int latestOne = doctorsOrders.size() - 1;
 		doctorsOrders.remove(latestOne);
 	}
 
+	/**
+	 * Returns the count of doctor's orders
+	 * 
+	 * @return Doctor's orders count
+	 */
 	public int getDoctorsOrdersCount() {
 		return doctorsOrders.size();
 	}
@@ -117,27 +171,28 @@ public class MedicalHistory {
 	/**
 	 * This method will export the medical history as a string.
 	 * It will start a new line and end with a new line.
-	 * @param appSchedule The schedule is required to get the information of appointments
+	 *
 	 * @return A string contains all the information of the medical history
 	 */
 	public String toString() {
-		String appointmentString = new String();
-		for(int i = 0; i<appointmentIDs.size();i++) {
+		String appointmentString = "";
+		for(int i = 0; i < appointmentIDs.size();i++) {
 			Appointment newApp = Schedule.getAppointment(appointmentIDs.get(i));
-			appointmentString = appointmentString + newApp.toString();
+			appointmentString += newApp.toString();
 		}
 		
-		String treatmentString = new String();
-		for(int i = 0; i<treatments.size() ; i++ ) {
-			treatmentString = treatmentString + treatments.get(i).toString();
+		String treatmentString = "";
+		for(int i = 0; i < treatments.size() ; i++ ) {
+			treatmentString += treatments.get(i).toString();
 		}
 		
-		String doctorsOrdersString = new String();
-		for(int i = 0; i<doctorsOrders.size(); i++) {
-			doctorsOrdersString = doctorsOrdersString + doctorsOrders.get(i).toString();
+		String doctorsOrdersString = "";
+		for(int i = 0; i < doctorsOrders.size(); i++) {
+			doctorsOrdersString += doctorsOrders.get(i).toString();
 		}
 		
-		String fullInfo = "\nMedical History\n\n\nAppointments:\n" + appointmentString +
+		final String fullInfo = "\nMedical History\n\n\nAppointments:\n" + 
+						  appointmentString +
 						  "\nTreatments:\n" + treatmentString +
 						  "\nDoctor's Orders:\n" + doctorsOrdersString;
 		return fullInfo;
