b6a3aca188c24c8ec3d80490509c0a625183e150
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-04 20:56:18 +0000

    the default content panel of patient

--- trunk/JavaBean/src/gui/PatientWelcomePanel.java
+++ trunk/JavaBean/src/gui/PatientWelcomePanel.java
@@ -1,6 +1,11 @@
 package gui;
 
 import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
 
 public class PatientWelcomePanel extends JPanel {
 
@@ -8,6 +13,19 @@ public class PatientWelcomePanel extends JPanel {
 	 * Create the panel.
 	 */
 	public PatientWelcomePanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblWelcome = new JLabel("Welcome!");
+		add(lblWelcome, "4, 4");
 
 	}
 
