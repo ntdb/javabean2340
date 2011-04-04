386070744189e96af1e803978153bfd30918fb5a
pyroalienmaniac <pyroalienmaniac@gmail.com>
2011-04-04 21:28:15 +0000

    Set Background for menus

--- JavaBean/src/gui/Breadcrumb.java
+++ JavaBean/src/gui/Breadcrumb.java
@@ -13,9 +13,11 @@ public class Breadcrumb {
 	private JPanel menu;
 	private JPanel content;
 	private JLabel label;
+	private final Color BG = new Color(212,212,232);
 	
 	public Breadcrumb(JPanel menuIn, JPanel contentIn, String title) {
 		menu = menuIn;
+		menu.setBackground(BG);
 		content = contentIn;
 		label = new JLabel(title);
 		label.setForeground(Color.BLUE);
