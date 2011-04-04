430e19559f9f75d99847b25943bd165e3cbf8220
halflifedave <halflifedave@gmail.com>
2011-04-04 04:17:22 +0000

    Treatment Panel Added

---
+++ JavaBean/src/gui/Treatment_Panel.java
@@ -0,0 +1,75 @@
+package gui;
+
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+
+public class Treatment_Panel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Treatment_Panel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblTreatingDocotr = new JLabel("Treating Doctor:");
+		add(lblTreatingDocotr, "2, 2");
+		
+		JLabel lblDate = new JLabel("Date:");
+		add(lblDate, "18, 2");
+		
+		JLabel lblAttendingNurse = new JLabel("Attending Nurse: ");
+		add(lblAttendingNurse, "2, 6");
+		
+		JLabel lblSymptoms = new JLabel("Symptoms:");
+		add(lblSymptoms, "2, 10");
+		
+		JLabel lblVitalSigns = new JLabel("Vital Signs:");
+		add(lblVitalSigns, "2, 14");
+		
+		JLabel lblDiagnosis = new JLabel("Diagnosis: ");
+		add(lblDiagnosis, "2, 18");
+
+	}
+
+}
