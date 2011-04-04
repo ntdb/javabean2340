9924b185c4f289ce834612766c26acfd8d700b77
halflifedave <halflifedave@gmail.com>
2011-04-04 20:02:47 +0000

    Audit Updates Started

--- JavaBean/src/gui/Invoice_Buttons.java
+++ JavaBean/src/gui/Invoice_Buttons.java
@@ -1,26 +1,40 @@
+/**
+ * GUI group of EMR Project
+ */
 package gui;
 
 import javax.swing.JPanel;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import com.jgoodies.forms.factories.FormFactory;
 import java.awt.Dimension;
 
+/**
+ * Panel is used for button inputs with the Invoice_Panels class.
+ * @author David
+ * @version 1.00
+ */
 public class Invoice_Buttons extends JPanel {
+	
+	/**
+	 * Width of the Invoice Buttons
+	 */
+	private final int wth;
+	
+	/**
+	 * Height of the Invoice Buttons
+	 */
+	private final int hgt;
 
 	/**
 	 * Create the panel.
 	 */
 	public Invoice_Buttons() {
+		wth = (int)1000;
+		hgt = (int)25;
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
-		JButton button = new JButton("Close Invoice");
-		button.setMaximumSize(new Dimension(1000, 25));
+		final JButton button = new JButton("Close Invoice");
+		button.setMaximumSize(new Dimension(wth, hgt));
 		add(button);
 
 	}
