dc152bbd7f7c73fb74ba865a4b4b2bce1c40cbe7
halflifedave@gmail.com <halflifedave@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-04 04:20:05 +0000

    Invoice Panel Added

---
+++ trunk/JavaBean/src/gui/Invoice_Panel.java
@@ -0,0 +1,59 @@
+package gui;
+
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+
+public class Invoice_Panel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Invoice_Panel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblName = new JLabel("Name: ");
+		add(lblName, "2, 2");
+		
+		JLabel lblDate = new JLabel("Date: ");
+		add(lblDate, "16, 2");
+		
+		JLabel lblDoctor = new JLabel("Doctor: ");
+		add(lblDoctor, "2, 6");
+		
+		JLabel lblBill = new JLabel("Bill: ");
+		add(lblBill, "2, 10");
+
+	}
+
+}
