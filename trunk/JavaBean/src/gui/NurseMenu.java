462cb52273571afe5b5e4d02e8a46406e26726a2
nathanaelbailey@gmail.com <nathanaelbailey@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-04 00:22:13 +0000

    Center aligned buttons, stretch to fit

--- trunk/JavaBean/src/gui/NurseMenu.java
+++ trunk/JavaBean/src/gui/NurseMenu.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.UIManager;
+import java.awt.Component;
+import java.awt.Dimension;
 
 public class NurseMenu extends JPanel {
 
@@ -14,12 +16,18 @@ public class NurseMenu extends JPanel {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		JButton button = new JButton("Patients");
+		button.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button.setMaximumSize(new Dimension(1000,25));
 		add(button);
 		
 		JButton button_1 = new JButton("Appointments");
+		button_1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button_1.setMaximumSize(new Dimension(1000,25));
 		add(button_1);
 		
 		JButton button_2 = new JButton("New Patient");
+		button_2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button_2.setMaximumSize(new Dimension(1000,25));
 		add(button_2);
 
 	}
