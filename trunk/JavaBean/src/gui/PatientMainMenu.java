9bb7b91742c70d2dfe2085132e055f1c68710014
nathanaelbailey@gmail.com <nathanaelbailey@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-04 19:06:18 +0000

    Some panel changes implemented. Some buttons work, others do not.

--- trunk/JavaBean/src/gui/PatientMainMenu.java
+++ trunk/JavaBean/src/gui/PatientMainMenu.java
@@ -6,6 +6,8 @@ import javax.swing.JButton;
 import javax.swing.UIManager;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PatientMainMenu extends JPanel {
 
@@ -21,21 +23,45 @@ public class PatientMainMenu extends JPanel {
 		add(btnProfile);
 		
 		JButton btnAppointments = new JButton("Appointments");
+		btnAppointments.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+//				Portal.update(new PatientAppointmentsMenu(), NEED PANEL HERE , "Appointments");
+			}
+		});
 		btnAppointments.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnAppointments.setMaximumSize(new Dimension(1000,25));
 		add(btnAppointments);
 		
 		JButton btnMedicalInformation = new JButton("Medical Information");
+		btnMedicalInformation.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+//				Portal.update(new PatientMedicalInformationMenu(), NEED PANEL HERE , "Medical Information");
+			}
+		});
 		btnMedicalInformation.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnMedicalInformation.setMaximumSize(new Dimension(1000,25));
 		add(btnMedicalInformation);
 		
 		JButton btnMedicalHistory = new JButton("Medical History");
+		btnMedicalHistory.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+//				Portal.update(new PatientMedicalHistoryMenu(), NEED PANEL HERE , "Medical History");
+			}
+		});
 		btnMedicalHistory.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnMedicalHistory.setMaximumSize(new Dimension(1000,25));
 		add(btnMedicalHistory);
 		
 		JButton btnInvoice = new JButton("Invoice");
+		btnInvoice.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				Portal.update(new Invoice_Buttons(), new Invoice_Panel(), "Invoice");
+			}
+		});
 		btnInvoice.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnInvoice.setMaximumSize(new Dimension(1000,25));
 		add(btnInvoice);
