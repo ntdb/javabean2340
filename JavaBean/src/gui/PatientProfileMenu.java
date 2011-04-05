014d2aaf16293e666b618fd9d86a3bb04004e98a
zhoubinwilliam <zhoubinwilliam@gmail.com>
2011-04-05 01:36:13 +0000

    Profile functions well

--- JavaBean/src/gui/PatientProfileMenu.java
+++ JavaBean/src/gui/PatientProfileMenu.java
@@ -6,6 +6,8 @@ import javax.swing.JButton;
 import javax.swing.UIManager;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PatientProfileMenu extends JPanel {
 
@@ -16,11 +18,23 @@ public class PatientProfileMenu extends JPanel {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		JButton btnChange = new JButton("Change Profile");
+		btnChange.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				Portal.setContent(new PatientProfileChangePanel());
+			}
+		});
 		btnChange.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnChange.setMaximumSize(new Dimension(1000,25));
 		add(btnChange);
 		
 		JButton btnRefresh = new JButton("Refresh");
+		btnRefresh.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Portal.setContent(new PatientProfilePanel());
+			}
+		});
 		btnRefresh.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnRefresh.setMaximumSize(new Dimension(1000,25));
 		add(btnRefresh);
