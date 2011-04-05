90ee84047b84830a590fd3819f225cf5727fbeea
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-05 01:51:18 +0000

    patient appointments default page built

--- trunk/JavaBean/src/gui/PatientAppointmentsPanel.java
+++ trunk/JavaBean/src/gui/PatientAppointmentsPanel.java
@@ -1,6 +1,14 @@
 package gui;
 
 import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+
+import emr.Patient;
+
+import javax.swing.JLabel;
 
 public class PatientAppointmentsPanel extends JPanel {
 
@@ -8,6 +16,25 @@ public class PatientAppointmentsPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public PatientAppointmentsPanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblNewLabel = new JLabel("You have " + ( (Patient)Portal.getUser() ).getMedicalHistory().getAppointmentsCount() + " appointments on record.");
+		add(lblNewLabel, "6, 8");
 
 	}
 
