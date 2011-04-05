5e1fa4ded11b8ff5f37809948e126728411b9ece
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-05 02:27:13 +0000

    appointment functions built

--- trunk/JavaBean/src/gui/PatientAppointmentsMenu.java
+++ trunk/JavaBean/src/gui/PatientAppointmentsMenu.java
@@ -6,6 +6,8 @@ import javax.swing.JButton;
 import javax.swing.UIManager;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PatientAppointmentsMenu extends JPanel {
 
@@ -16,11 +18,23 @@ public class PatientAppointmentsMenu extends JPanel {
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		JButton btnRequest = new JButton("Request Appointment");
+		btnRequest.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Portal.setContent(new PatientAppointmentsRequestPanel());
+			}
+		});
 		btnRequest.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnRequest.setMaximumSize(new Dimension(1000,25));
 		add(btnRequest);
 		
 		JButton btnView = new JButton("View Appointments");
+		btnView.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Portal.setContent(new PatientAppointmentsViewPanel());
+			}
+		});
 		btnView.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnView.setMaximumSize(new Dimension(1000,25));
 		add(btnView);
