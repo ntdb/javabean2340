08251a687e2cc440da51a565ec7ac18f2b5b9f5c
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-05 02:39:14 +0000

    medical information page done

--- trunk/JavaBean/src/gui/PatientMedicalInformationPanel.java
+++ trunk/JavaBean/src/gui/PatientMedicalInformationPanel.java
@@ -1,6 +1,11 @@
 package gui;
 
 import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
 
 public class PatientMedicalInformationPanel extends JPanel {
 
@@ -8,6 +13,35 @@ public class PatientMedicalInformationPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public PatientMedicalInformationPanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblNewLabel = new JLabel("Pharmacy: <the pharmacy information will be shown here>");
+		add(lblNewLabel, "4, 6");
+		
+		JLabel lblNewLabel_1 = new JLabel("Insurance Carrier: <the insurance carrier will be shown here>");
+		add(lblNewLabel_1, "4, 10");
+		
+		JLabel lblNewLabel_2 = new JLabel("Allergies: <the allergies information will be shown here>");
+		add(lblNewLabel_2, "4, 14");
 
 	}
 
