e9ee88bef1c702e3089f6dfc7fae705931ca680e
zhoubinwilliam <zhoubinwilliam@gmail.com>
2011-04-05 02:26:53 +0000

    appointments list page setup, more implementation needed

---
+++ JavaBean/src/gui/PatientAppointmentsViewPanel.java
@@ -0,0 +1,42 @@
+package gui;
+
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+
+public class PatientAppointmentsViewPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PatientAppointmentsViewPanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblThisPageWill = new JLabel("This page will show a list of appontments...");
+		add(lblThisPageWill, "6, 12");
+
+	}
+
+}
