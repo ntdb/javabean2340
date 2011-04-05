46cdf5bd59e446c13b00d135a2782c48c0d34b34
pyroalienmaniac <pyroalienmaniac@gmail.com>
2011-04-05 01:32:33 +0000

    Basic Nurse view Patient screen

---
+++ JavaBean/src/gui/ViewPatientContent.java
@@ -0,0 +1,69 @@
+package gui;
+
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class ViewPatientContent extends JPanel {
+
+	
+	private JLabel lblName;
+	
+	/**
+	 * Create the panel.
+	 */
+	public ViewPatientContent() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblPatientInformation = new JLabel("Patient Information");
+		lblPatientInformation.setFont(new Font("Lucida Grande", Font.BOLD, 28));
+		add(lblPatientInformation, "10, 4");
+		
+		lblName = new JLabel("Name");
+		lblName.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
+		add(lblName, "10, 10");
+
+	}
+
+	public JLabel getLblName() {
+		return lblName;
+	}
+	
+	
+
+}
