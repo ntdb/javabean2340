1a9abc26fb574c9afe70232df2759f73d991bc8c
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-05 02:43:45 +0000

    medical history default page done

--- trunk/JavaBean/src/gui/PatientMedicalHistoryPanel.java
+++ trunk/JavaBean/src/gui/PatientMedicalHistoryPanel.java
@@ -1,6 +1,14 @@
 package gui;
 
 import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+
+import emr.Patient;
+
+import javax.swing.JLabel;
 
 public class PatientMedicalHistoryPanel extends JPanel {
 
@@ -8,6 +16,32 @@ public class PatientMedicalHistoryPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public PatientMedicalHistoryPanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblNewLabel = new JLabel("You have " + ( (Patient)Portal.getUser() ).getMedicalHistory().getTreatmentCount() + " treatment records.");
+		add(lblNewLabel, "6, 8");
+		
+		JLabel lblNewLabel_1 = new JLabel("You have " + ( (Patient)Portal.getUser() ).getMedicalHistory().getDoctorsOrdersCount() + " doctors orders on record.");
+		add(lblNewLabel_1, "6, 12");
 
 	}
 
