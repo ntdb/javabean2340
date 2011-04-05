ca1dd6485ca5abe238b1f8e1330db130959c1db9
zhoubinwilliam <zhoubinwilliam@gmail.com>
2011-04-05 02:26:19 +0000

    appointments request panel finished

---
+++ JavaBean/src/gui/PatientAppointmentsRequestPanel.java
@@ -0,0 +1,111 @@
+package gui;
+
+import javax.swing.JPanel;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class PatientAppointmentsRequestPanel extends JPanel {
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Create the panel.
+	 */
+	public PatientAppointmentsRequestPanel() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		JLabel lblAllTheInformation = new JLabel("All information below is required to request an appointment.");
+		add(lblAllTheInformation, "2, 6, 17, 1");
+		
+		JLabel lblNewLabel = new JLabel("Date(MM/DD/YYYY):");
+		add(lblNewLabel, "4, 10, right, default");
+		
+		textField = new JTextField();
+		add(textField, "6, 10, 9, 1, fill, default");
+		textField.setColumns(10);
+		
+		JLabel label = new JLabel("Doctor's Name:");
+		add(label, "4, 14");
+		
+		textField_1 = new JTextField();
+		add(textField_1, "6, 14, 9, 1, fill, default");
+		textField_1.setColumns(10);
+		
+		final JLabel lblChange = new JLabel("");
+		lblChange.setForeground(Color.RED);
+		lblChange.setFont(new Font("Lucida Grande", Font.ITALIC, 13));
+		add(lblChange, "4, 18, 15, 1");
+
+		JButton btnSend = new JButton("Send");
+		btnSend.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				lblChange.setText("Your request has been sent!");
+			}
+		});
+		add(btnSend, "14, 22");
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Portal.setContent(new PatientAppointmentsPanel());
+			}
+		});
+		add(btnCancel, "16, 22");
+
+	}
+}
