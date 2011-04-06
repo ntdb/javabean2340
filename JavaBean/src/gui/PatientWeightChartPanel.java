c39ea87773e29cb20a1264da2e29b82d6234591b
nathanaelbailey <nathanaelbailey@gmail.com>
2011-04-06 23:27:35 +0000

    Added chart libraries (add them to your build path) and implemented sample weight chart for 4000

--- JavaBean/src/gui/PatientWeightChartPanel.java
+++ JavaBean/src/gui/PatientWeightChartPanel.java
@@ -1,41 +1,48 @@
 package gui;
 
+import java.util.Date;
+
 import javax.swing.JPanel;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.factories.FormFactory;
 import javax.swing.JLabel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.time.Day;
+import org.jfree.data.time.TimeSeries;
+import org.jfree.data.time.TimeSeriesCollection;
+
 
 public class PatientWeightChartPanel extends JPanel {
-
+	
 	/**
 	 * Create the panel.
 	 */
 	public PatientWeightChartPanel() {
-		setLayout(new FormLayout(new ColumnSpec[] {
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,
-				FormFactory.RELATED_GAP_COLSPEC,
-				FormFactory.DEFAULT_COLSPEC,},
-			new RowSpec[] {
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,
-				FormFactory.RELATED_GAP_ROWSPEC,
-				FormFactory.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblNewLabel = new JLabel("Patient's weight chart will be shown here.");
-		add(lblNewLabel, "6, 12");
+		TimeSeries series = new TimeSeries("", Day.class);
+		series.add(new Day(new Date("2011/03/04")), 150);
+		series.add(new Day(new Date("2011/03/05")), 145);
+		series.add(new Day(new Date("2011/03/06")), 160);
+		
+		TimeSeriesCollection dataset = new TimeSeriesCollection();
+		dataset.addSeries(series);
+		
+		JFreeChart chart = ChartFactory.createTimeSeriesChart
+		("Patient weight chart",		//Could be modified to show name
+		 "Date",
+		 "Weight",
+		 dataset,
+		 true,
+		 true,
+		 false
+		);
+		
+		add(new ChartPanel(chart));
 
 	}
 
