e29c4636e32e31cb4ecd5ccfd8b6cc5c1564d889
zhoubinwilliam@gmail.com <zhoubinwilliam@gmail.com@5c88bab8-2274-2993-5452-321df29e3827>
2011-04-07 22:19:36 +0000

    patient medical information shown correctly

--- trunk/JavaBean/src/gui/PatientMedicalInformationPanel.java
+++ trunk/JavaBean/src/gui/PatientMedicalInformationPanel.java
@@ -5,6 +5,9 @@ import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
 import com.jgoodies.forms.factories.FormFactory;
+
+import emr.Patient;
+
 import javax.swing.JLabel;
 
 public class PatientMedicalInformationPanel extends JPanel {
@@ -34,13 +37,13 @@ public class PatientMedicalInformationPanel extends JPanel {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblNewLabel = new JLabel("Pharmacy: <the pharmacy information will be shown here>");
+		JLabel lblNewLabel = new JLabel("Pharmacy: " + ((Patient)Portal.getUser()).getPharmacy() );
 		add(lblNewLabel, "4, 6");
 		
-		JLabel lblNewLabel_1 = new JLabel("Insurance Carrier: <the insurance carrier will be shown here>");
+		JLabel lblNewLabel_1 = new JLabel("Insurance Carrier: " + ((Patient)Portal.getUser()).getInsuranceCarrier());
 		add(lblNewLabel_1, "4, 10");
 		
-		JLabel lblNewLabel_2 = new JLabel("Allergies: <the allergies information will be shown here>");
+		JLabel lblNewLabel_2 = new JLabel("Allergies: " + ((Patient)Portal.getUser()).getAllergies());
 		add(lblNewLabel_2, "4, 14");
 
 	}
