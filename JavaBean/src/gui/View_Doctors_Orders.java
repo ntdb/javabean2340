8d22ad2c7c48ae9882d170ca9df8fb83190df006
halflifedave <halflifedave@gmail.com>
2011-04-07 18:01:14 +0000

    View Doctors Orders Panel Added

---
+++ JavaBean/src/gui/View_Doctors_Orders.java
@@ -0,0 +1,66 @@
+package gui;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import com.jgoodies.forms.factories.FormFactory;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.RowSpec;
+
+public class View_Doctors_Orders extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public View_Doctors_Orders() {
+		setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,}));
+		
+		final JLabel lblFollowupInstructions = new JLabel("Follow-up Instructions");
+		add(lblFollowupInstructions, "2, 12");
+		
+		JLabel followup = new JLabel("");
+		Doctors_Orders doc = new Doctors_Orders();
+		String result = doc.getFollowup();
+		followup.setText(result);
+		add(followup, "4, 14, 9, 3");
+		
+		final JLabel lblOtherInstructions = new JLabel("Other Instructions: ");
+		add(lblOtherInstructions, "2, 18");
+
+	}
+
+}
