a3b2cd1700f7573de9f1509755f759e0835512e5
halflifedave <halflifedave@gmail.com>
2011-04-07 18:00:28 +0000

    View Doc Orders Created. Doctor Doc Orders Updated. Attempting to display results in view Doc Orders Panel

--- JavaBean/src/gui/Doctors_Orders.java
+++ JavaBean/src/gui/Doctors_Orders.java
@@ -12,8 +12,16 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 public class Doctors_Orders extends JPanel {
-	private JTextField textField;
-	private JTextField textField_1;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private final JTextField followup;
+	
+	private final JTextField other;
+	
+	private final int colsize;
 
 	/**
 	 * Create the panel.
@@ -54,34 +62,36 @@ public class Doctors_Orders extends JPanel {
 				FormFactory.RELATED_GAP_ROWSPEC,
 				FormFactory.DEFAULT_ROWSPEC,}));
 		
-		JLabel lblPrescriptions = new JLabel("Prescriptions:");
+		colsize = 10;
+		
+		final JLabel lblPrescriptions = new JLabel("Prescriptions:");
 		add(lblPrescriptions, "2, 2");
 		
-		JCheckBox chckbxDrugA = new JCheckBox("Drug A");
+		final JCheckBox chckbxDrugA = new JCheckBox("Drug A");
 		add(chckbxDrugA, "4, 4");
 		
-		JCheckBox chckbxDrugB = new JCheckBox("Drug B");
+		final JCheckBox chckbxDrugB = new JCheckBox("Drug B");
 		add(chckbxDrugB, "8, 4");
 		
-		JCheckBox chckbxDrugC = new JCheckBox("Drug C");
+		final JCheckBox chckbxDrugC = new JCheckBox("Drug C");
 		add(chckbxDrugC, "12, 4");
 		
-		JLabel lblTestPerformed = new JLabel("Lab Work Performed: ");
+		final JLabel lblTestPerformed = new JLabel("Lab Work Performed: ");
 		add(lblTestPerformed, "2, 8");
 		
-		JCheckBox chckbxMri = new JCheckBox("MRI");
+		final JCheckBox chckbxMri = new JCheckBox("MRI");
 		add(chckbxMri, "4, 10");
 		
-		JCheckBox chckbxCatScan = new JCheckBox("Cat Scan");
+		final JCheckBox chckbxCatScan = new JCheckBox("Cat Scan");
 		add(chckbxCatScan, "8, 10");
 		
-		JCheckBox chckbxXray = new JCheckBox("X-Ray");
+		final JCheckBox chckbxXray = new JCheckBox("X-Ray");
 		add(chckbxXray, "12, 10");
 		
-		JLabel lblFollowupInstructions = new JLabel("Follow-up Instructions");
+		final JLabel lblFollowupInstructions = new JLabel("Follow-up Instructions");
 		add(lblFollowupInstructions, "2, 12");
 		
-		final JTextField followup = new JTextField();
+		followup = new JTextField();
 		followup.getDocument().addDocumentListener(new DocumentListener() {
 			public void changedUpdate(DocumentEvent e) {
 			// text was changed, implement a way to save this
@@ -97,15 +107,31 @@ public class Doctors_Orders extends JPanel {
 			}
 			});
 		add(followup, "4, 14, 9, 2, fill, default");
-		followup.setColumns(10);
+		followup.setColumns(colsize);
 		
-		JLabel lblOtherInstructions = new JLabel("Other Instructions: ");
+		final JLabel lblOtherInstructions = new JLabel("Other Instructions: ");
 		add(lblOtherInstructions, "2, 18");
 		
-		JTextField instruction = new JTextField();
-		add(instruction, "4, 19, 9, 2, fill, default");
-		instruction.setColumns(10);
+		other = new JTextField();
+		add(other, "4, 19, 9, 2, fill, default");
+		other.setColumns(colsize);
 
+	}
+	
+	/**
+	 * Returns the follow up String entered
+	 * @return followup String
+	 */
+	public String getFollowup(){
+		return followup.getText();
+	}
+	
+	/**
+	 * Returns the other instruction String entered
+	 * @return other instructions
+	 */
+	public String getOther(){
+		return other.getText();
 	}
 
 }
